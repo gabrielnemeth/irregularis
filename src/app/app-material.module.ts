64a4218d4f1178f6fb71ca91453b1339dd6d254f
gabrielnemeth <github.zaphrentid@aleeas.com>
2021-08-29 16:42:42 +0200

    Adjust the settings page

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -3,8 +3,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-    exports: [MatIconModule, MatInputModule, MatButtonModule, MatListModule],
+    exports: [
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatListModule,
+        MatCardModule,
+    ],
 })
 export class AppMaterialModule {}
